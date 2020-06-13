18a3057dab44182f862bc5377e3351eda9337414
yirano <yirano@protonmail.ch>
2020-06-13 11:59:29 -0400

    Add more challenges

--- index.js
+++ index.js
@@ -1,78 +1,108 @@
-// What is an Array?
+// REVIEW What is an Array?
 
-// What is an Object? 
+// REVIEW What is an Object?
 
-// How would I grab 'cherries' from the following array?
+// TODO How would I grab 'cherries' from the following array?
 
-let fruits = ['apple', 'banana', 'cherries', 'watermelon', 'oranges']
+// let fruits = ['apple', 'banana', 'cherries', 'watermelon', 'oranges']
 
-// Let's create an object of three users: Bob, Sara, and Yvette with their name, hobbies, and favorite color
+// TODO Let's create an object of three users: Bob, Sara, and Yvette with their name, hobbies, and favorite color
 
-let users;
+// let users
 
-// console.log Bob's favorite color
+// TODO console.log Bob's favorite color
 
-// console.log Sara's second hobby
+// TODO console.log Sara's second hobby
 
-// What is a function? And why is it important?
+// REVIEW What is a function? And why is it important?
 
-// Create a function that iterates through the following array and multiplies it by 2
+// TODO Create a function that iterates through the following array and multiplies it by 2
 
-let multiplyThis = [1, 3, 5, 7, 9]
+// REVIEW for-loops
 
-// Use a method to split the following String value
+// let multiplyThis = [1, 3, 5, 7, 9]
 
-let splitThis = 'We’re in this together, from your first day of classes to your first day on the job — and beyond.'
+// TODO Use a method to split the following String value
 
-// console.log the length of the split String
+// let splitThis = 'We’re in this together, from your first day of classes to your first day on the job — and beyond.'
 
-// Challenge: Create a function that finds the smallest and largest number in an array
+// TODO console.log the length of the split String
 
-let arr1 = [1, 2, 3, 4, 5]
-let arr2 = [6, 3, 2, 64, 7]
-function minMax(arr) {
-  return arr
-}
+// TODO Challenge: Create a function that finds the smallest and largest number in an array
 
-console.log('Largest Number in array --> ', minMax(arr1))
+// let arr1 = [1, 2, 3, 4, 5]
+// let arr2 = [6, 3, 2, 64, 7]
+// function minMax(arr) {
+//   return arr
+// }
 
-// Challenge: Create a function that filters out strings from array
-let str1 = [1, 5, 'hi', 9, true, 'bye']
+// console.log('Largest Number in array --> ', minMax(arr1))
 
-function filterArray(arr) {
-	return arr
-}
+// TODO Challenge: Create a function that filters out strings from array
+// let str1 = [1, 5, 'hi', 9, true, 'bye']
 
-console.log('FilterArray --> ', filterArray(str1))
+// function filterArray(arr) {
 
-// Challenge: Sort array by string length
-let sortThis = ["Google", "Apple", "Microsoft"]
-let sortThis2 = ["Turing", "Einstein", "Jung"]
+// }
 
-function sortByLength(arr) {
-  
-  return arr
-}
+// console.log('FilterArray --> ', filterArray(str1))
 
-console.log('Sort by String length --> ', sortByLength(arr))
+// TODO Challenge: What is the largest number?
+// let findLargest = [5, 3, 8, 3, 1, 8, 45]
+// function largestNum(num) {
+//   return num
+// }
 
-// Challenge: Convert Key, Values in an Object to Array
+// console.log('Largest Num -->', largestNum(findLargest))
 
-const obj = {
-  D: 1,
-  B: 2,
-  C: 3
-}
+// TODO Challenge: Sort array by string length
 
-const obj2 = {
-  likes: 2,
-  dislikes: 3,
-  followers: 10
-}
+// let sortThis = ["Google", "Apple", "Microsoft", "Turing", "Einstein", "Jung"]
+// let sortThis2 = ["Turing", "Einstein", "Jung"]
 
-function objectToArray(obj) {
-  return obj
-}
+// function sortByLength(arr) {
 
-console.log('Object to Array', objectToArray(obj))
+//   return arr
+// }
+// console.log('Sort by String length --> ', sortByLength(sortThis))
 
+
+// TODO Challenge Check if number is in a given range
+// const range1 = {
+//   min: 1,
+//   max: 5
+// }
+
+// function isInRange(num, range) {
+
+// }
+
+// console.log('Is in range --> ', isInRange(3, range1))
+
+
+//TODO Phone number formatting
+
+// const alicePhone = 6524137523
+
+// function numberFormat(number) {
+//   return number
+// }
+
+// console.log('Number Format --> ', numberFormat(alicePhone))
+
+
+// TODO 
+
+// const reviews = [{ name: "Daniela", rating: 5, feedback: "Beautiful atmosphere and wonderful vegan options!" },
+// { name: "Jack", rating: 3, feedback: "A little too hipster for my taste, but the burger was decent, if overpriced" },
+// { name: "Miranda", rating: 4, feedback: "fun trivia and cool vibes" },
+// { name: "Wen", rating: 4.5, feedback: "I don't leave my house often, but when I do, it's for this place. Highly reccomend." },
+// { name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day." },
+// { name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
+// { name: "Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay." },
+// { name: "Reyna", rating: 3.5, feedback: "" },
+// ]
+
+// function getLongReviews(/* code here */) {
+//   /* code here */
+// }
