0aa0c4cc636f196be9ffc65bef9870595db81cb5
Tiffany Arellano <missusfantastic@Missus-Fantastic.local>
2020-06-13 00:34:29 -0400

    Set up for SH lecture

---
+++ index.js
@@ -0,0 +1,29 @@
+// What is an Array?
+
+// What is an Object? 
+
+// How would I grab 'cherries' from the following array?
+
+let fruits = ['apple', 'banana', 'cherries', 'watermelon', 'oranges']
+
+// Let's create an object of three users: Bob, Sara, and Yvette with their name, hobbies, and favorite color
+
+let users;
+
+// console.log Bob's favorite color
+
+// console.log Sara's second hobby
+
+// What is a function? And why is it important?
+
+// Create a function that iterates through the following array and multiplies it by 2
+
+let multiplyThis = [1, 3, 5, 7, 9]
+
+// Use a method to split the following String value
+
+let splitThis = 'I do not like green eggs and ham!'
+
+// console.log the length of the split String
+
+
