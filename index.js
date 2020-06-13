c8c424b3a0ea1c8d716cb93238e8b055293a5210
Tiffany Arellano <missusfantastic@Missus-Fantastic.local>
2020-06-13 01:16:49 -0400

    Add challenges

--- index.js
+++ index.js
@@ -22,8 +22,57 @@ let multiplyThis = [1, 3, 5, 7, 9]
 
 // Use a method to split the following String value
 
-let splitThis = 'I do not like green eggs and ham!'
+let splitThis = 'We’re in this together, from your first day of classes to your first day on the job — and beyond.'
 
 // console.log the length of the split String
 
+// Challenge: Create a function that finds the smallest and largest number in an array
+
+let arr1 = [1, 2, 3, 4, 5]
+let arr2 = [6, 3, 2, 64, 7]
+function minMax(arr) {
+  return arr
+}
+
+console.log('Largest Number in array --> ', minMax(arr1))
+
+// Challenge: Create a function that filters out strings from array
+let str1 = [1, 5, 'hi', 9, true, 'bye']
+
+function filterArray(arr) {
+	return arr
+}
+
+console.log('FilterArray --> ', filterArray(str1))
+
+// Challenge: Sort array by string length
+let sortThis = ["Google", "Apple", "Microsoft"]
+let sortThis2 = ["Turing", "Einstein", "Jung"]
+
+function sortByLength(arr) {
+  
+  return arr
+}
+
+console.log('Sort by String length --> ', sortByLength(arr))
+
+// Challenge: Convert Key, Values in an Object to Array
+
+const obj = {
+  D: 1,
+  B: 2,
+  C: 3
+}
+
+const obj2 = {
+  likes: 2,
+  dislikes: 3,
+  followers: 10
+}
+
+function objectToArray(obj) {
+  return obj
+}
+
+console.log('Object to Array', objectToArray(obj))
 
